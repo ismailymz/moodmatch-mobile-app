33aa1a1fd8ccc8591d261b7f69348889e424d260
ismailymz <ismailmktr25@gmail.com>
2026-05-26 14:08:16 +0200

    chore: add project files; ignore .metadata and editor settings

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,14 @@
+class AppConstants {
+  AppConstants._();
+
+  static const String appName = 'MoodMatch';
+  static const String contentJsonPath = 'assets/data/content.json';
+
+  static const String homeRoute = '/';
+  static const String browseRoute = '/browse';
+  static const String detailRoute = '/detail';
+  static const String searchRoute = '/search';
+  static const String favouritesRoute = '/favourites';
+  static const String settingsRoute = '/settings';
+  static const String aboutRoute = '/about';
+}
