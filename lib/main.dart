3f79dbac18b091aa50ab6801485e214696c29b18
ismailymz <ismailmktr25@gmail.com>
2026-05-27 16:32:32 +0200

    Connect app router in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'constants/app_constants.dart';
-import 'screens/search_screen.dart';
+import 'router/app_router.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,46 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: AppConstants.appName,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(AppConstants.appName),
-        ),
-        body: const _Home(),
-      ),
-    );
-  }
-}
-
-class _Home extends StatelessWidget {
-  const _Home();
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Text('Welcome to MoodMatch'),
-          const SizedBox(height: 16),
-          ElevatedButton.icon(
-            icon: const Icon(Icons.search),
-            label: const Text('Open Search'),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => const SearchScreen(),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
+    return MaterialApp.router(
+      title: 'MoodMatch',
+      debugShowCheckedModeBanner: false,
+      routerConfig: appRouter,
     );
   }
 }
